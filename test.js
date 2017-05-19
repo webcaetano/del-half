e1f0ba88429baac986ce4e9f21b712713127f601
Andre Caetano <webandrecaetano@gmail.com>
2017-05-19 19:31:45 -0300

    new test

--- test.js
+++ test.js
@@ -27,3 +27,26 @@ test.cb('should delete half files',function(t){
 		t.end();
 	});
 });
+
+test.cb('should delete half files by even',function(t){
+	var src = './test/dist/wizz/**/*.png';
+	var dest = './test/dist/wizz';
+
+	del.sync('./test/dist/');
+	fs.copySync('./test/wizz-origin',dest);
+
+	var inputFiles = glob.sync(src,{nodir:true});
+	var numOfInput = inputFiles.length;
+
+	self(src,{
+		type:'even'
+	},function(err,data){
+		var outputFiles = glob.sync(dest+'/**',{nodir:true});
+		var numOutput = outputFiles.length;
+
+		t.truthy(numOutput<=Math.ceil(numOfInput/2));
+		t.falsy(err);
+		t.pass();
+		t.end();
+	});
+});
